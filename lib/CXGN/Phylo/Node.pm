6406b22ed593cd6b787e31f63260c6af35b13b7e
Robert Buels <rmb32@cornell.edu>
2010-08-24 16:01:09 -0700

    warning squash

--- lib/CXGN/Phylo/Node.pm
+++ lib/CXGN/Phylo/Node.pm
@@ -1960,6 +1960,7 @@ sub recursive_generate_newick {
 	my @children = @{$self->{children}};
 	
 	if (@children) { 
+            no warnings 'uninitialized';
 	    $s.="(";	
 	    for (my $i=0; $i<@children; $i++) {
 		$s =$children[$i]->recursive_generate_newick($s,'',$show_root);	   
