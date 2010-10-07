eba9369cdece160fef430988bd4233b8986d39fb
mueller <lam87@cornell.edu>
2010-10-07 12:08:56 -0400

    adding some POD and fixing the minimum length reported bug

--- lib/CXGN/Tools/GetGaps.pm
+++ lib/CXGN/Tools/GetGaps.pm
@@ -1,4 +1,23 @@
 
+=head1 NAME
+
+CXGN::Tools::GetGaps - a script to obtain information about gaps (indicated as N's) in a fasta file (such as the tomato genome)
+
+=head1 DESCRIPTION
+
+Implemented as a MooseX::Runnable script to be run as follows:
+
+mx-run CXGN::Tools::GetGaps --fasta_file sequences.fasta --min_gap_size 20 > output.txt
+
+=head1 AUTHOR
+
+Lukas Mueller
+
+
+=cut
+
+
+
 package CXGN::Tools::GetGaps;
 use Moose;
 
@@ -47,7 +66,7 @@ sub run {
 		if ($n_region_start) { $n_region_end = $i; }
 
 		my $gap_size = $n_region_end - $n_region_start + 1;
-		if ($gap_size > $self->min_gap_size()) { 
+		if ($gap_size >= $self->min_gap_size()) { 
 
 		    print "$id\_"; printf "%06d", "$gap_no"; print "\t$id\t$n_region_start\t$n_region_end\t$gap_size\n";
 		    $gap_no++;
