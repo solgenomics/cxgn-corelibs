b52bcd1351c6ce1c85b520045e29b371b8d246ee
Naama Menda <nm249@cornell.edu>
2011-08-25 08:49:33 -0400

    added an option for organism function to return a hash

--- lib/CXGN/Tools/Organism.pm
+++ lib/CXGN/Tools/Organism.pm
@@ -16,12 +16,14 @@ Functions for accessing ogranism names and their identifiers
 
 =head2 get_all_organisms
 
- Usage:        my ($names_ref, $ids_ref) = CXGN::Tools::Organism::get_all_organisms($dbh);
+ Usage:        my ($names_ref, $ids_ref) = CXGN::Tools::Organism::get_all_organisms($dbh, %return_hash_flag);
  Desc:         This is a static function. Retrieves distinct organism names and IDs from sgn.common_name
  Ret:          Returns two arrayrefs. One array contains all the
                organism names, and the other all the organism ids
-               with corresponding array indices.
- Args:         a database handle
+               with corresponding array indices
+               or a hash {common_name}=common_name_id if 2nd argument is provided
+
+ Args:         a database handle, a boolean (optional)
  Side Effects:
  Example:
 
@@ -35,9 +37,9 @@ use warnings;
 
 
 
-
 sub get_all_organisms {
     my $dbh = shift;
+    my $return_hash = shift;
     #this query should be changed to work with chado's organism table after sgn.common_name is replaced with it.
     my $query = "SELECT common_name, common_name_id 
                    FROM sgn.common_name ORDER BY upper(common_name) desc";
@@ -45,10 +47,13 @@ sub get_all_organisms {
     $sth->execute();
     my @names = ();
     my @ids = ();
+     my %common_names=();
     while (my($common_name, $common_name_id) = $sth->fetchrow_array()) { 
 	push @names, $common_name;
 	push @ids, $common_name_id;
+	$common_names{$common_name}= $common_name_id;
     }
+    return %common_names if $return_hash;
     return (\@names, \@ids);
 }
 
@@ -57,12 +62,11 @@ sub get_all_organisms {
 
  Usage:        my ($names_ref, $ids_ref) = CXGN::Tools::Organism::get_existing_organisms($dbh , return_hash_flag);
  Desc:         This is a static function. Selects the distinct organism names and their IDs from phenome.locus.
-               Useful fro populating a unique drop-down menu with only the organism names that exist in the table.
+               Useful for populating a unique drop-down menu with only the organism names that exist in the table.
  Ret:          Returns two arrayrefs. One array contains all the
-               organism names, and the other all the organism ids
+               organism names, and the other all the organism ids with corresponding array indices.
               or a hash {common_name}=common_name_id if 2nd argument is provided
-               with corresponding array indices.
- Args:         a database handle
+ Args:         a database handle, and a boolean (optional)
  Side Effects:
  Example:
 
