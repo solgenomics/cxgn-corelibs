907d13b6f1f6ac4b33c332806647a771cec503cb
Lukas Mueller <lam87@cornell.edu>
2019-09-25 16:03:50 -0400

    change alive function to call a script on the cluster using ssh to determine job state.

--- lib/CXGN/Tools/Run/Plugin/RemoteSlurm.pm
+++ lib/CXGN/Tools/Run/Plugin/RemoteSlurm.pm
@@ -404,77 +404,93 @@ sub _diefile_exists {
 sub alive {
     my $self = shift;
 
-    print STDERR "Slurm alive()... JobID: ".$self->jobid()."\n";
-
+    my $jobid = $self->jobid();
+    my $temp_dir = $self->temp_dir();
     
+    print STDERR "Slurm alive()... JobID: $jobid\n";
 
-    my $slurm = Slurm::new();
+    my $submit_host = $self->submit_host();
+    my $submit_host_prefix = "";
+    if ($submit_host) {
+	$submit_host_prefix = "ssh $submit_host ";
+    }
     
-    my $job_info = $slurm->load_job($self->cluster_job_id());
-    my $current_job = $job_info->{job_array}->[0];
+    my $output = `$submit_host_prefix check_slurm_job.pl $jobid $temp_dir 2>&1`;
+
+    if ($output == 1) {
+	return 1;
+    }
+    else {
+	return 0;
+    }
+
+    # my $slurm = Slurm::new();
     
-    $self->_check_nodes_states();
+    # my $job_info = $slurm->load_job($self->cluster_job_id());
+    # my $current_job = $job_info->{job_array}->[0];
+    
+    # $self->_check_nodes_states();
 
-    print STDERR "Check job state...\n";
+    # print STDERR "Check job state...\n";
 
-    if (IS_JOB_RUNNING($current_job)) {
-        print STDERR "Slurm job is running...\n";
-        return 1;
-    }
-    if (IS_JOB_COMPLETE($current_job)) {
-        print STDERR "slurm job is complete...\n";
-        return;
-    }
-    if (IS_JOB_FINISHED($current_job)) {
-        print STDERR "Slurm job is finished...\n";
-        return;
-    }
-    if (IS_JOB_COMPLETED($current_job)) {
-        print STDERR "Slurm job is completed...\n";
-        return;
-    }
-    if (IS_JOB_PENDING($current_job)) {
-        print STDERR "Slurm job is pending...\n";
-        return 1;
-    }
-    if (IS_JOB_COMPLETING($current_job)) {
-        print STDERR "Slurm job is completing...\n";
-        return 1;
-    }
-    if (IS_JOB_CONFIGURING($current_job)) {
-        print STDERR "Slurm job is configuring...\n";
-        return 1;
-    }
-    if (IS_JOB_STARTED($current_job)) {
-        print STDERR "Slurm job is started...\n";
-        return 1;
-    }
-    if (IS_JOB_RESIZING($current_job)) {
-        print STDERR "Slurm job is resizing...\n";
-        return 1;
-    }
-    if (IS_JOB_SUSPENDED($current_job)) {
-        die "Slurm job is suspended...\n";
-    }
-    if (IS_JOB_CANCELLED($current_job)) {
-        die "Slurm job is canceled...\n";
-    }
-    if (IS_JOB_FAILED($current_job)) {
-        die "Slurm job is failed...\n";
-	write_file(File::Spec->catfile($self->job_tempdir(), "died"), "Slurm job failed\n");
-    }
-    if (IS_JOB_TIMEOUT($current_job)) {
-        die "Slurm job is timed out...\n";
-	write_file(File::Spec->catfile($self->job_tempdir(), "died"), "Slurm job timed out\n");
-    }
-    if (IS_JOB_NODE_FAILED($current_job)) {
-        die "Slurm job node failed...\n";
-	write_file(File::Spec->catfile($self->job_tempdir(), "died"), "Slurm job node failed\n");
-    }
+    # if (IS_JOB_RUNNING($current_job)) {
+    #     print STDERR "Slurm job is running...\n";
+    #     return 1;
+    # }
+    # if (IS_JOB_COMPLETE($current_job)) {
+    #     print STDERR "slurm job is complete...\n";
+    #     return;
+    # }
+    # if (IS_JOB_FINISHED($current_job)) {
+    #     print STDERR "Slurm job is finished...\n";
+    #     return;
+    # }
+    # if (IS_JOB_COMPLETED($current_job)) {
+    #     print STDERR "Slurm job is completed...\n";
+    #     return;
+    # }
+    # if (IS_JOB_PENDING($current_job)) {
+    #     print STDERR "Slurm job is pending...\n";
+    #     return 1;
+    # }
+    # if (IS_JOB_COMPLETING($current_job)) {
+    #     print STDERR "Slurm job is completing...\n";
+    #     return 1;
+    # }
+    # if (IS_JOB_CONFIGURING($current_job)) {
+    #     print STDERR "Slurm job is configuring...\n";
+    #     return 1;
+    # }
+    # if (IS_JOB_STARTED($current_job)) {
+    #     print STDERR "Slurm job is started...\n";
+    #     return 1;
+    # }
+    # if (IS_JOB_RESIZING($current_job)) {
+    #     print STDERR "Slurm job is resizing...\n";
+    #     return 1;
+    # }
+    # if (IS_JOB_SUSPENDED($current_job)) {
+    #     die "Slurm job is suspended...\n";
+    # }
+    # if (IS_JOB_CANCELLED($current_job)) {
+    #     die "Slurm job is canceled...\n";
+    # }
+    # if (IS_JOB_FAILED($current_job)) {
+    #     die "Slurm job is failed...\n";
+    # 	write_file(File::Spec->catfile($self->job_tempdir(), "died"), "Slurm job failed\n");
+    # }
+    # if (IS_JOB_TIMEOUT($current_job)) {
+    #     die "Slurm job is timed out...\n";
+    # 	write_file(File::Spec->catfile($self->job_tempdir(), "died"), "Slurm job timed out\n");
+    # }
+    # if (IS_JOB_NODE_FAILED($current_job)) {
+    #     die "Slurm job node failed...\n";
+    # 	write_file(File::Spec->catfile($self->job_tempdir(), "died"), "Slurm job node failed\n");
+    # }
 
-    $self->_die_if_error;
+    # $self->_die_if_error;
 
-    die "Slurm job is in an unknown state...\n";
+    # die "Slurm job is in an unknown state...\n";
 
 }
 
@@ -535,7 +551,6 @@ sub _check_nodes_states {
             print STDERR "Slurm node is in power save... Node: ".$_->{name}."\n";
         }
     }
-
     return;
 }
 
@@ -567,7 +582,14 @@ sub out {
 sub cancel { 
     my $self = shift;
 
-    system('scancel', $self->cluster_job_id());
+    my $submit_host = $self->submit_host();
+    my $submit_host_prefix = "";
+    if ($submit_host) {
+	$submit_host_prefix = "ssh $submit_host ";
+    }
+
+    my $jobid = $self->cluster_job_id();
+    my @output = `$submit_host_prefix scancel $jobid`;
 }
 
 # sub serialize { 
